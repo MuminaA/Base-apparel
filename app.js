bb7e9bb221cb821e36b9bc9eaa39ec533dc045e8
mumina <muminaabdi13@gmail.com>
2023-02-01 00:08:51 -0500

    javascript setup, stuck with error

---
+++ app.js
@@ -0,0 +1,15 @@
+/* document.addEventListener("DOMContentLoaded", function () {
+  function ValidateEmail() {
+    let mail = document.getElementById("text").value;
+
+    let validRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+
+    if (validRegex.text(mail)) {
+      alert("Valid email address!");
+      return true;
+    } else {
+      alert("Invalid email address!");
+      return false;
+    }
+  }
+}); */
