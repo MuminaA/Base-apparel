55bc494cb064a8d6427b199dc141e5f39f27cff4
mumina <muminaabdi13@gmail.com>
2023-02-01 17:11:27 -0500

    javascript intergrated

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-  function ValidateEmail() {
-    let mail = document.getElementById("text").value;
+let input = document.querySelector("input");
+const emailRegex = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
 
-    let validRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+const form_input = document.querySelector("form");
+const error_logo = document.querySelector("#error_logo");
+const invalid_text = document.querySelector("#invalid_text");
 
-    if (validRegex.text(mail)) {
-      alert("Valid email address!");
-      return true;
-    } else {
-      alert("Invalid email address!");
-      return false;
-    }
+document.querySelector("#button").addEventListener("click", function () {
+  if (!emailRegex.test(input.value)) {
+    error_logo.style.display = "block";
+    form_input.style.border = "1px solid red";
+    invalid_text.style.display = "block";
+  } else {
+    error_logo.style.display = "none";
+    invalid_text.style.display = "none";
+    form_input.style.border = "1px solid hsla(0, 36%, 70%, 0.288)";
   }
-}); */
+});
